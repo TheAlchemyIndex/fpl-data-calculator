4c06a32541d8d1a50449f1bac5ba2c232f113bf9
TheAlchemyIndex <adam.vaughan@hotmail.co.uk>
2023-05-02 16:58:31 +0100

    fixing calculation bug in RollingAverage

--- src/test/scala/unifiedData/RollingAverageTests.scala
+++ src/test/scala/unifiedData/RollingAverageTests.scala
@@ -30,12 +30,14 @@ class RollingAverageTests extends TestHelper {
       ("name1", Date.valueOf("2019-08-13"), 5),
       ("name1", Date.valueOf("2019-08-14"), 6),
       ("name1", Date.valueOf("2019-08-15"), 7),
+      ("name1", Date.valueOf("2019-08-16"), 8),
       ("name2", Date.valueOf("2019-08-10"), 2),
       ("name2", Date.valueOf("2019-08-11"), 3),
       ("name2", Date.valueOf("2019-08-12"), 4),
       ("name2", Date.valueOf("2019-08-13"), 6),
       ("name2", Date.valueOf("2019-08-14"), 7),
       ("name2", Date.valueOf("2019-08-15"), 8),
+      ("name2", Date.valueOf("2019-08-16"), 9)
     ), List(
       (NAME_COL, StringType, true),
       (DATE_COL, DateType, true),
@@ -45,18 +47,20 @@ class RollingAverageTests extends TestHelper {
 
   val EXPECTED_INTEGER_AVG_DF: DataFrame = SPARK.createDF(
     List(
-      ("name1", Date.valueOf("2019-08-10"), 1, 1.0),
-      ("name1", Date.valueOf("2019-08-11"), 2, 1.5),
-      ("name1", Date.valueOf("2019-08-12"), 3, 2.0),
-      ("name1", Date.valueOf("2019-08-13"), 5, 2.75),
-      ("name1", Date.valueOf("2019-08-14"), 6, 3.4),
-      ("name1", Date.valueOf("2019-08-15"), 7, 4.6),
-      ("name2", Date.valueOf("2019-08-10"), 2, 2.0),
-      ("name2", Date.valueOf("2019-08-11"), 3, 2.5),
-      ("name2", Date.valueOf("2019-08-12"), 4, 3.0),
-      ("name2", Date.valueOf("2019-08-13"), 6, 3.75),
-      ("name2", Date.valueOf("2019-08-14"), 7, 4.4),
-      ("name2", Date.valueOf("2019-08-15"), 8, 5.6),
+      ("name1", Date.valueOf("2019-08-10"), 1, null),
+      ("name1", Date.valueOf("2019-08-11"), 2, 1.0),
+      ("name1", Date.valueOf("2019-08-12"), 3, 1.5),
+      ("name1", Date.valueOf("2019-08-13"), 5, 2.0),
+      ("name1", Date.valueOf("2019-08-14"), 6, 2.75),
+      ("name1", Date.valueOf("2019-08-15"), 7, 3.4),
+      ("name1", Date.valueOf("2019-08-16"), 8, 4.6),
+      ("name2", Date.valueOf("2019-08-10"), 2, null),
+      ("name2", Date.valueOf("2019-08-11"), 3, 2.0),
+      ("name2", Date.valueOf("2019-08-12"), 4, 2.5),
+      ("name2", Date.valueOf("2019-08-13"), 6, 3.0),
+      ("name2", Date.valueOf("2019-08-14"), 7, 3.75),
+      ("name2", Date.valueOf("2019-08-15"), 8, 4.4),
+      ("name2", Date.valueOf("2019-08-16"), 9, 5.6)
     ), List(
       (NAME_COL, StringType, true),
       (DATE_COL, DateType, true),
@@ -73,12 +77,14 @@ class RollingAverageTests extends TestHelper {
       ("name1", Date.valueOf("2019-08-13"), 8.3),
       ("name1", Date.valueOf("2019-08-14"), 10.4),
       ("name1", Date.valueOf("2019-08-15"), 12.5),
+      ("name1", Date.valueOf("2019-08-16"), 14.6),
       ("name2", Date.valueOf("2019-08-10"), 1.0),
       ("name2", Date.valueOf("2019-08-11"), 3.1),
       ("name2", Date.valueOf("2019-08-12"), 5.2),
       ("name2", Date.valueOf("2019-08-13"), 7.3),
       ("name2", Date.valueOf("2019-08-14"), 9.4),
       ("name2", Date.valueOf("2019-08-15"), 11.5),
+      ("name2", Date.valueOf("2019-08-16"), 13.6)
     ), List(
       (NAME_COL, StringType, true),
       (DATE_COL, DateType, true),
@@ -88,18 +94,20 @@ class RollingAverageTests extends TestHelper {
 
   val EXPECTED_DOUBLE_AVG_DF: DataFrame = SPARK.createDF(
     List(
-      ("name1", Date.valueOf("2019-08-10"), 2.0, 2.0),
-      ("name1", Date.valueOf("2019-08-11"), 4.1, 3.05),
-      ("name1", Date.valueOf("2019-08-12"), 6.2, 4.1000000000000005),
-      ("name1", Date.valueOf("2019-08-13"), 8.3, 5.15),
-      ("name1", Date.valueOf("2019-08-14"), 10.4, 6.2),
-      ("name1", Date.valueOf("2019-08-15"), 12.5, 8.3),
-      ("name2", Date.valueOf("2019-08-10"), 1.0, 1.0),
-      ("name2", Date.valueOf("2019-08-11"), 3.1, 2.05),
-      ("name2", Date.valueOf("2019-08-12"), 5.2, 3.1),
-      ("name2", Date.valueOf("2019-08-13"), 7.3, 4.15),
-      ("name2", Date.valueOf("2019-08-14"), 9.4, 5.2),
-      ("name2", Date.valueOf("2019-08-15"), 11.5, 7.3),
+      ("name1", Date.valueOf("2019-08-10"), 2.0, null),
+      ("name1", Date.valueOf("2019-08-11"), 4.1, 2.0),
+      ("name1", Date.valueOf("2019-08-12"), 6.2, 3.05),
+      ("name1", Date.valueOf("2019-08-13"), 8.3, 4.1000000000000005),
+      ("name1", Date.valueOf("2019-08-14"), 10.4, 5.15),
+      ("name1", Date.valueOf("2019-08-15"), 12.5, 6.2),
+      ("name1", Date.valueOf("2019-08-16"), 14.6, 8.3),
+      ("name2", Date.valueOf("2019-08-10"), 1.0, null),
+      ("name2", Date.valueOf("2019-08-11"), 3.1, 1.0),
+      ("name2", Date.valueOf("2019-08-12"), 5.2, 2.05),
+      ("name2", Date.valueOf("2019-08-13"), 7.3, 3.1),
+      ("name2", Date.valueOf("2019-08-14"), 9.4, 4.15),
+      ("name2", Date.valueOf("2019-08-15"), 11.5, 5.2),
+      ("name2", Date.valueOf("2019-08-16"), 13.6, 7.3)
     ), List(
       (NAME_COL, StringType, true),
       (DATE_COL, DateType, true),
@@ -116,12 +124,14 @@ class RollingAverageTests extends TestHelper {
       ("name1", Date.valueOf("2019-08-13"), 845L),
       ("name1", Date.valueOf("2019-08-14"), 1060L),
       ("name1", Date.valueOf("2019-08-15"), 1275L),
+      ("name1", Date.valueOf("2019-08-16"), 1490L),
       ("name2", Date.valueOf("2019-08-10"), 100L),
       ("name2", Date.valueOf("2019-08-11"), 305L),
       ("name2", Date.valueOf("2019-08-12"), 510L),
       ("name2", Date.valueOf("2019-08-13"), 715L),
       ("name2", Date.valueOf("2019-08-14"), 920L),
       ("name2", Date.valueOf("2019-08-15"), 1125L),
+      ("name2", Date.valueOf("2019-08-16"), 1330L)
     ), List(
       (NAME_COL, StringType, true),
       (DATE_COL, DateType, true),
@@ -131,18 +141,20 @@ class RollingAverageTests extends TestHelper {
 
   val EXPECTED_LONG_AVG_DF: DataFrame = SPARK.createDF(
     List(
-      ("name1", Date.valueOf("2019-08-10"), 200L, 200.0),
-      ("name1", Date.valueOf("2019-08-11"), 410L, 305.0),
-      ("name1", Date.valueOf("2019-08-12"), 625L, 411.6666666666667),
-      ("name1", Date.valueOf("2019-08-13"), 845L, 520.0),
-      ("name1", Date.valueOf("2019-08-14"), 1060L, 628.0),
-      ("name1", Date.valueOf("2019-08-15"), 1275L, 843.0),
-      ("name2", Date.valueOf("2019-08-10"), 100L, 100.0),
-      ("name2", Date.valueOf("2019-08-11"), 305L, 202.5),
-      ("name2", Date.valueOf("2019-08-12"), 510L, 305.0),
-      ("name2", Date.valueOf("2019-08-13"), 715L, 407.5),
-      ("name2", Date.valueOf("2019-08-14"), 920L, 510.0),
-      ("name2", Date.valueOf("2019-08-15"), 1125L, 715.0),
+      ("name1", Date.valueOf("2019-08-10"), 200L, null),
+      ("name1", Date.valueOf("2019-08-11"), 410L, 200.0),
+      ("name1", Date.valueOf("2019-08-12"), 625L, 305.0),
+      ("name1", Date.valueOf("2019-08-13"), 845L, 411.6666666666667),
+      ("name1", Date.valueOf("2019-08-14"), 1060L, 520.0),
+      ("name1", Date.valueOf("2019-08-15"), 1275L, 628.0),
+      ("name1", Date.valueOf("2019-08-16"), 1490L, 843.0),
+      ("name2", Date.valueOf("2019-08-10"), 100L, null),
+      ("name2", Date.valueOf("2019-08-11"), 305L, 100.0),
+      ("name2", Date.valueOf("2019-08-12"), 510L, 202.5),
+      ("name2", Date.valueOf("2019-08-13"), 715L, 305.0),
+      ("name2", Date.valueOf("2019-08-14"), 920L, 407.5),
+      ("name2", Date.valueOf("2019-08-15"), 1125L, 510.0),
+      ("name2", Date.valueOf("2019-08-16"), 1330L, 715.0)
     ), List(
       (NAME_COL, StringType, true),
       (DATE_COL, DateType, true),
@@ -234,9 +246,7 @@ class RollingAverageTests extends TestHelper {
 
   test("applyRollingAvg - It should return a DataFrame containing rolling averages for all relevant columns") {
     val applyRollingAvgDf: DataFrame = applyRollingAvg(TEST_JOINED_DF, NUM_OF_ROWS)
-    applyRollingAvgDf.show()
-    EXPECTED_JOINED_AVG_DF.show()
-//    assert(EXPECTED_JOINED_AVG_DF.schema === applyRollingAvgDf.schema)
-//    assert(EXPECTED_JOINED_AVG_DF.collect().sameElements(applyRollingAvgDf.collect()))
+    assert(EXPECTED_JOINED_AVG_DF.schema === applyRollingAvgDf.schema)
+    assert(EXPECTED_JOINED_AVG_DF.collect().sameElements(applyRollingAvgDf.collect()))
   }
 }
