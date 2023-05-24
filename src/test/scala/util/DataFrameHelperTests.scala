afedb2d46f30b7ed97834e8519a5747c4f00328d
TheAlchemyIndex <adam.vaughan@hotmail.co.uk>
2023-05-24 12:52:21 +0100

    fixing DataFrame comparison in DataFrameHelperTests

--- src/test/scala/util/DataFrameHelperTests.scala
+++ src/test/scala/util/DataFrameHelperTests.scala
@@ -11,11 +11,14 @@ class DataFrameHelperTests extends TestHelper {
   final val COL1_SNAKE_CASE = "col_num1"
   final val COL2_SNAKE_CASE = "col_num2"
   final val COL3_SNAKE_CASE = "col_num3"
+
   final val COL1_CAMEL_CASE = "colNum1"
   final val COL2_CAMEL_CASE = "colNum2"
   final val COL3_CAMEL_CASE = "colNum3"
+
   final val WAS_HOME_COL: String = "wasHome"
   final val HOME_FIXTURE_COL: String = "homeFixture"
+
   val EXPECTED_CAMEL_CASE_COLS: Array[String] = Array("colNum1", "colNum2", "colNum3")
 
   val TEST_SNAKE_CASE_DF: DataFrame = SPARK.createDF(
@@ -181,43 +184,57 @@ class DataFrameHelperTests extends TestHelper {
 
   test("renameColumns - It should return a DataFrame with snake case column headers converted to camelCase") {
     val camelCaseDf: DataFrame = renameColumnsToCamelCase(TEST_SNAKE_CASE_DF)
-    assert(EXPECTED_CAMEL_CASE_DF.schema === camelCaseDf.schema)
-    assert(EXPECTED_CAMEL_CASE_DF.collect().sameElements(camelCaseDf.collect()))
+    val dfDifferences = camelCaseDf.exceptAll(EXPECTED_CAMEL_CASE_DF)
+
+    assert(EXPECTED_CAMEL_CASE_DF.schema === camelCaseDf.schema, "The schemas of camelCaseDf and EXPECTED_CAMEL_CASE_DF did not match")
+    assert(dfDifferences.count() == 0, "The rows of camelCaseDf and EXPECTED_CAMEL_CASE_DF did not match")
   }
 
   test("renameColumns - It should return a DataFrame with camel case column headers unchanged") {
     val camelCaseDf: DataFrame = renameColumnsToCamelCase(TEST_CAMEL_CASE_DF)
-    assert(EXPECTED_CAMEL_CASE_DF.schema === camelCaseDf.schema)
-    assert(EXPECTED_CAMEL_CASE_DF.collect().sameElements(camelCaseDf.collect()))
+    val dfDifferences = camelCaseDf.exceptAll(EXPECTED_CAMEL_CASE_DF)
+
+    assert(EXPECTED_CAMEL_CASE_DF.schema === camelCaseDf.schema, "The schemas of camelCaseDf and EXPECTED_CAMEL_CASE_DF did not match")
+    assert(dfDifferences.count() == 0, "The rows of camelCaseDf and EXPECTED_CAMEL_CASE_DF did not match")
   }
 
   test("booleanColumnToBinary - It should return a DataFrame containing a new homeFixture column with 1 or 0 values") {
     val binaryDf: DataFrame = booleanColumnToBinary(TEST_BOOLEAN_DF, HOME_FIXTURE_COL, WAS_HOME_COL)
-    assert(EXPECTED_BINARY_DF.schema === binaryDf.schema)
-    assert(EXPECTED_BINARY_DF.collect().sameElements(binaryDf.collect()))
+    val dfDifferences = binaryDf.exceptAll(EXPECTED_BINARY_DF)
+
+    assert(EXPECTED_BINARY_DF.schema === binaryDf.schema, "The schemas of binaryDf and EXPECTED_BINARY_DF did not match")
+    assert(dfDifferences.count() == 0, "The rows of binaryDf and EXPECTED_BINARY_DF did not match")
   }
 
   test("booleanColumnToBinary - null values - It should return a DataFrame containing a new homeFixture column with 0 values") {
     val binaryDf: DataFrame = booleanColumnToBinary(TEST_BOOLEAN_DF_NULL_VALUES, HOME_FIXTURE_COL, WAS_HOME_COL)
-    assert(EXPECTED_BINARY_DF_NULL_VALUES.schema === binaryDf.schema)
-    assert(EXPECTED_BINARY_DF_NULL_VALUES.collect().sameElements(binaryDf.collect()))
+    val dfDifferences = binaryDf.exceptAll(EXPECTED_BINARY_DF_NULL_VALUES)
+
+    assert(EXPECTED_BINARY_DF_NULL_VALUES.schema === binaryDf.schema, "The schemas of binaryDf and EXPECTED_BINARY_DF_NULL_VALUES did not match")
+    assert(dfDifferences.count() == 0, "The rows of binaryDf and EXPECTED_BINARY_DF_NULL_VALUES did not match")
   }
 
   test("dropNullRows - It should return a DataFrame containing no rows with null values") {
     val droppedNullRowsDf: DataFrame = dropNullRows(TEST_NULL_ROWS_DF, Seq(COL2_CAMEL_CASE))
-    assert(EXPECTED_NULL_ROWS_DROPPED_DF.schema === droppedNullRowsDf.schema)
-    assert(EXPECTED_NULL_ROWS_DROPPED_DF.collect().sameElements(droppedNullRowsDf.collect()))
+    val dfDifferences = droppedNullRowsDf.exceptAll(EXPECTED_NULL_ROWS_DROPPED_DF)
+
+    assert(EXPECTED_NULL_ROWS_DROPPED_DF.schema === droppedNullRowsDf.schema, "The schemas of droppedNullRowsDf and EXPECTED_NULL_ROWS_DROPPED_DF did not match")
+    assert(dfDifferences.count() == 0, "The rows of droppedNullRowsDf and EXPECTED_NULL_ROWS_DROPPED_DF did not match")
   }
 
   test("joinDataLeftOuter - Single Column - It should join 2 DataFrames together on a single column and return the joined DataFrame") {
     val joinedDf: DataFrame = joinDataLeftOuter(TEST_LEFT_SINGLE_DF, TEST_RIGHT_SINGLE_DF, Seq(COL1_CAMEL_CASE))
-    assert(EXPECTED_JOINED_SINGLE_DF.schema === joinedDf.schema)
-    assert(EXPECTED_JOINED_SINGLE_DF.collect().sameElements(joinedDf.collect()))
+    val dfDifferences = joinedDf.exceptAll(EXPECTED_JOINED_SINGLE_DF)
+
+    assert(EXPECTED_JOINED_SINGLE_DF.schema === joinedDf.schema, "The schemas of joinedDf and EXPECTED_JOINED_SINGLE_DF did not match")
+    assert(dfDifferences.count() == 0, "The rows of joinedDf and EXPECTED_JOINED_SINGLE_DF did not match")
   }
 
   test("joinDataLeftOuter - Two Columns - It should join 2 DataFrames together on 2 columns and return the joined DataFrame") {
     val joinedDf: DataFrame = joinDataLeftOuter(TEST_LEFT_DOUBLE_DF, TEST_RIGHT_DOUBLE_DF, Seq(WAS_HOME_COL, COL1_CAMEL_CASE))
-    assert(EXPECTED_JOINED_DOUBLE_DF.schema === joinedDf.schema)
-    assert(EXPECTED_JOINED_DOUBLE_DF.collect().sameElements(joinedDf.collect()))
+    val dfDifferences = joinedDf.exceptAll(EXPECTED_JOINED_DOUBLE_DF)
+
+    assert(EXPECTED_JOINED_DOUBLE_DF.schema === joinedDf.schema, "The schemas of joinedDf and EXPECTED_JOINED_DOUBLE_DF did not match")
+    assert(dfDifferences.count() == 0, "The rows of joinedDf and EXPECTED_JOINED_DOUBLE_DF did not match")
   }
 }
